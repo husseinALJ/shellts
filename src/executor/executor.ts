fb736fb9b6f7510265db369597c5aa4b07e4cd2c
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-09-05 23:00:57 +0300

    Add: implement type command functionality and handle it in executeProgram

--- src/executor/executor.ts
+++ src/executor/executor.ts
@@ -1,6 +1,26 @@
+import path from "node:path";
 import { rl } from "../main.js";
+import fs from "fs";
 
 import type { BuiltIns, CommandAST } from "../types/main.js";
+
+const findCommandPath = (command: string) => {
+  const Path = process.env.PATH;
+  const pathArr = Path?.split(path.delimiter);
+  const commandPath = pathArr?.find((dir) => {
+    try {
+      const itsHere = fs.readdirSync(dir).includes(command);
+      if (itsHere) {
+        fs.accessSync(path.join(dir, command), fs.constants.X_OK);
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  });
+  if (commandPath) return commandPath;
+  return;
+};
 
 export const builtinCommands: BuiltIns = {
   exit: (): void => {
@@ -24,10 +44,21 @@ export const builtinCommands: BuiltIns = {
   echo: (text: string): void => {
     console.log(text?.replaceAll("'", ""));
   },
+  type: (params: string): string => {
+    if (params.trim() === "") return "";
+
+    if (params in builtinCommands) return "builtIn";
+
+    const commandPath = findCommandPath(params) || "";
+    return commandPath;
+  },
 };
 
 export const executeProgram = (program: CommandAST): void => {
-  if (program[0].type === "builtIn")
-    return builtinCommands[program[0].value]?.(program[1]);
+  if (program[0].type === "builtIn") {
+    const results = builtinCommands[program[0].value as keyof BuiltIns](program[1]);
+    if (typeof results === "string") return console.log(results);
+    return;
+  }
   console.log(`${program[0].value}: command not found`);
 };
