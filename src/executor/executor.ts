69a928ab92b1c289467236157e4969143f558645
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-09-05 21:08:46 +0300

    Add: executePrograms and BuiltIns type

--- src/executor/executor.ts
+++ src/executor/executor.ts
@@ -1,1 +1,9 @@
-export const builtinCommands = {};
+import type { BuiltIns, CommandAST } from "../types/main.js";
+
+export const builtinCommands: BuiltIns = {};
+
+export const executeProgram = (program: CommandAST): void => {
+  if (program[0].type === "builtIn")
+    return builtinCommands[program[0].value]?.(program[1]);
+  console.log(`${program[0].value}: command not found`);
+};
--- src/types/main.ts
+++ src/types/main.ts
@@ -1,3 +1,5 @@
 export type Token = [command: string , params: string]
 
 export type CommandAST = [ {type: string, value: string}, params: string ]
+
+export type BuiltIns = {[commands: string]: (params?: string) => void}
