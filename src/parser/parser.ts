bf0449440bffa3e2e8e31ba78493b8bba0984a9a
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-09-05 23:02:29 +0300

    refactor: simplify command type determination in parser

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,33 +1,5 @@
-import path from "node:path";
-import fs from "fs";
-
 import type { CommandAST, Token } from "../types/main.js";
 import { builtinCommands } from "../executor/executor.js";
-
-const findCommandPath = (command: string) => {
-  const Path = process.env.PATH;
-  const pathArr = Path?.split(path.delimiter);
-  const commandPath = pathArr?.find((dir) => {
-    try {
-      const itsHere = fs.readdirSync(dir).includes(command);
-      if (itsHere) {
-        fs.accessSync(path.join(dir, command), fs.constants.X_OK);
-        return true;
-      }
-    } catch (err) {
-      return false;
-    }
-  });
-  if (commandPath) return commandPath;
-  return;
-};
-
-const getCommandType = (command: string): { type: string; value: string } => {
-  if (command in builtinCommands) return { type: "builtIn", value: command };
-  const commandPath = findCommandPath(command);
-  if (commandPath) return { type: commandPath, value: command };
-  return { type: "", value: command };
-};
 
 const quotesHandler = (quotedText: string) => {
   return quotedText.split("'").reduce((accumulator, e, index) => {
@@ -44,7 +16,7 @@ const paramsFormatter = (params: string) => {
 };
 
 export const parser = (token: Token): CommandAST => {
-  const commandType = getCommandType(token[0]);
+  const commandType = builtinCommands.type(token[0]);
   const params = paramsFormatter(token[1]);
-  return [commandType, params];
+  return [{type: commandType, value: token[0]}, params];
 };
