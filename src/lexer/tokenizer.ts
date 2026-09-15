87f485b0238c0fcd3c903e3ed4e6724ff2fb51f3
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-09-15 21:02:27 +0300

    refactor: tokenizer for quoted commands

--- src/lexer/tokenizer.ts
+++ src/lexer/tokenizer.ts
@@ -1,9 +1,21 @@
 import type { Token } from "../types/main.js";
 
+const commandTokenizer = (text: string): string => {
+  const quotesChars = new Set(["'", '"']);
+  if (quotesChars.has(text?.charAt(0))) {
+    const command = text.match(/(["'])(.*?)\1/);
+
+    if (command) {
+      return command[0];
+    }
+  }
+  return text.split(" ")[0]!;
+};
+
 export const tokenize = (input: string): Token => {
   const trimmedInput = input.trim();
-  const command = trimmedInput.split(" ")[0]!;
+  const command = commandTokenizer(trimmedInput);
   const parameters = trimmedInput.replace(`${command}`, "").trim();
   const token: Token = [command, parameters];
-  return token
+  return token;
 };
